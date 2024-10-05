07daa2147de28d51eb135113c2cc1ca7d78de791
Sara <ammisara2004@gmail.com>
2024-10-05 23:35:49 +0200

    multiple operations fonctionality added

--- src/main.c
+++ src/main.c
@@ -4,13 +4,14 @@
 #include <string.h>
 #include <ctype.h>
 #include "types.h"
-#include "interpret.h"
+#include "lexer.h"
 
 int main () {
+    while (true) {
     // valeur d'entrée temp
-    char calcul[20]; 
+    char calcul[50]; 
     printf("give us an operation : "); 
-    fgets(calcul , 20 , stdin); 
+    fgets(calcul , 50 , stdin); 
     printf("this %s \n", calcul); 
 
     // initialiser le nom de la variable à 'vide'
@@ -30,69 +31,25 @@ int main () {
     strcat(variable , temp); 
     i++; 
     }
-    
-    // ignorer les espaces 
-    while (calcul[i] == '=' || calcul[i] == ' ') {
-    i++; 
-    }
-    
-    char * first_int = malloc (sizeof(char)*20); 
-    first_int[0] = '\0';
 
-    while (isdigit(calcul[i])) {
-    char temp[2]; 
-    temp[0] = calcul [i]; 
-    temp[1] = '\0'; 
-    strcat(first_int, temp); 
-    i++; 
-    }
-
-    // créer le token de int 1
-    Token *head = add_token_number (NULL , atoi(first_int)); 
-    Token * tail = head ; 
-
-    // ignorer les espaces 
-    i++ ; 
-    while ( calcul[i] == ' ' || calcul[i] == '=') {
-    i++; 
-    }
-    
-    // ajouter le token de int 2
-    tail = add_token_operator (tail , calcul[i]); 
-
-    // ignorer les espaces 
-    i++ ;
-    while ( calcul[i] == ' ') {
-    i++; 
-    }
-
-    char * second_int = malloc (sizeof(char)*20); 
-    second_int[0] = '\0';
-
-    while (isdigit(calcul[i])) {
-    char temp[2]; 
-    temp[0] = calcul [i]; 
-    temp[1] = '\0'; 
-    strcat(second_int, temp); 
-    i++; 
-    }
-    
-    // ajouter le token de int 2
-    tail = add_token_number (tail , atoi(second_int));
+    Token *head = get_operation_list (calcul); 
     
     // calculer 
-    int nb = get_result(head); 
+    int result = head->my_token.number ; 
+    get_result(head->next , &result); 
 
     // afficher le resultat 
-    printf("this %s = %d \n ", variable, nb); 
+    printf("this %s = %d \n", variable, result); 
     free(variable); 
-    free(first_int); 
+
     
     // free des malloc 
     while (head != NULL) {
         Token *next = head->next;
         free(head);
+
         head = next;
     } 
+    }
     return 0 ; 
 }
