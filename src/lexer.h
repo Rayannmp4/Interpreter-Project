50b485922033d9d4809aedd15c3cf1d278fb477c
Sara-Ammi <ammisara2004@gmail.com>
2024-10-27 01:38:56 +0200

    AST and Result done

--- src/lexer.h
+++ src/lexer.h
@@ -1,12 +1,9 @@
-#ifndef LEXER_H
-#define LEXER_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 #include "types.h"
-
 
 bool is_operator ( char c ) { 
     if ( c == '+' || c == '-' 
@@ -21,8 +18,80 @@ bool is_valid_var_char(char c) {
     return (isalpha(c) || c == '_');
 }
 
+Token * add_token_number (Token * head , char num ) {
+    Token * token = (Token *) malloc (sizeof(Token)); 
+    token->type = NUMBER ; 
+    token->my_token.number = num ; 
+    token->next = NULL ; 
+    if (head == NULL) {
+    return token ; 
+    } else {
+    head->next = token ; 
+    return token ; 
+    }
+}
 
-Token * get_operation_list ( char * operation ) {
+Token * add_token_operator (Token * head , char op ) {
+    Token * token = (Token *) malloc (sizeof(Token)); 
+    token->type = OPERATOR ; 
+    token->my_token.op = op ; 
+    if (op == '+' || op == '-') {
+        token->periorite = 1 ; 
+    } else {
+        token->periorite = 2 ; 
+    }
+
+    token->next = NULL ; 
+    if (head == NULL) {
+    return token ; 
+    } else {
+    head->next = token ; 
+    return token ; 
+    }
+}
+
+Token * add_token_var(Token * head, const char *var_name) {
+    Token * token = (Token *) malloc(sizeof(Token));
+    token->type = TOKEN_VAR;
+    strcpy(token->my_token.var_name, var_name);
+    token->next = NULL;
+    if (head == NULL) {
+        return token;
+    } else {
+        head->next = token;
+        return token;
+    }
+}
+
+Token * add_token_openB(Token * head, char  open) {
+    Token * token = (Token *) malloc(sizeof(Token));
+    token->type = OPEN_BRACKET;
+    token->my_token.b_open = open ; 
+    token->next = NULL;
+    if (head == NULL) {
+        return token;
+    } else {
+        head->next = token;
+        return token;
+    }
+}
+
+Token * add_token_closeB(Token * head, char  close) {
+    Token * token = (Token *) malloc(sizeof(Token));
+    token->type = CLOSE_BRACKET;
+    token->my_token.b_close = close ; 
+    token->next = NULL;
+    if (head == NULL) {
+        return token;
+    } else {
+        head->next = token;
+        return token;
+    }
+}
+
+
+
+Token * lexer ( char * operation ) {
 
 Token * head = NULL ;
 Token * tail = NULL ; 
@@ -75,13 +144,29 @@ while (operation[i] != '\0') {
             }
             continue; // Revenir au début du while
         }
+
     if (is_operator(operation[i])) {
     tail = add_token_operator (tail , operation[i]);
-    
     i++ ; 
+    continue;
+    } 
+
+    if (operation[i] == '(') {  
+    if (tail == NULL) {
+                head =  add_token_openB (tail , operation[i]);
+                tail = head;
+            } else {
+                tail =  add_token_openB (tail , operation[i]); 
+    }
+    i++ ; 
+    continue;
+    }
+
+    if (operation[i] == ')') {
+    tail = add_token_closeB (tail , operation[i]);
+    i++ ; 
+    continue;
     }
 }
 return head ; 
 }
-
-#endif 
